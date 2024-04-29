fc1d765f8c1ea99fb0889853a2fef97332272a6b
Vucuongtuan <vucuongtuan00@gmail.com>
2024-04-29 10:30:50 +0700

    fvhgcv

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"UaNZflsyXeTV3Ebgt4YkLaTM8pEOwrjNqiIP0A0ugeM=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"+al/mWnusKXcVn/zJMiO0C8LwKS13VBo5eOYooWmFbc=\"}"
--- src/app/details/sitemap.ts
+++ src/app/details/sitemap.ts
@@ -3,17 +3,11 @@ import { MetadataRoute } from "next";
 import { getAllData } from "@/api/product/index.api";
 import http from "@/utils/axios";
 
-export async function generateSitemaps() {
-  return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }];
-}
-
 export default async function sitemap({
   id,
 }: {
   id: number;
 }): Promise<MetadataRoute.Sitemap> {
-  const start = id * 50000;
-  const end = start + 50000;
   const products = await getAllData();
   return products.map((product: any) => ({
     url: `${process.env.BASE_URL_SERVER}/product/${product._id}`,
